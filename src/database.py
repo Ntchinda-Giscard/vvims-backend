7ae8e019e13f987729aae3ea17be076c6a1da925
Ntchinda-Giscard <ntchinda1998@gmail.com>
2021-01-01 03:15:03 +0100

    commit

--- src/database.py
+++ src/database.py
@@ -9,6 +9,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("Url db: ", DATABASE_URL)
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
