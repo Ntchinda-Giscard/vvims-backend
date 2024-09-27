0adffbc01a9bf50f504611e2b93a2d937f801bac
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-09-27 18:11:58 +0100

    add data

--- app.py
+++ app.py
@@ -125,14 +125,14 @@ async def visits_trigger(body: Dict):
     with next(get_db()) as db:
         try:
 
-            # db_notif = EmployeeNotification(
-            #     action = "ADD VISITOR",
-            #     title = "New Visitor Alert !",
-            #     message="A new visitor has been! Click here to see more details",
-            #     is_read = False
-            # )
-            # db.add(db_notif)
-            # db.commit()
+            db_notif = EmployeeNotification(
+                action = "ADD VISITOR",
+                title = "New Visitor Alert !",
+                message="A new visitor has been! Click here to see more details",
+                is_read = False
+            )
+            db.add(db_notif)
+            db.commit()
 
         except Exception as e:
             db.rollback()
