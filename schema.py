a4d08d28939724289e92c0409fd0d5664800c0ae
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-10-25 14:47:49 +0100

    upload

--- schema.py
+++ schema.py
@@ -90,12 +90,11 @@ class Query:
                 
                 else:
                     print("No employees were present.")
-                    date_time_attendance = None
-                date_time_attendance = DayAttendanceType(
-                    date= f"\nDate: {date.strftime('%Y-%m-%d')}",
-                    attendance = attend
-                )
-            return date_time_attendance
+                    
+            return DayAttendanceType(
+                        date= f"\nDate: {date.strftime('%Y-%m-%d')}",
+                        attendance = attend
+                    )
 
 
 @strawberry.type
