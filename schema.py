74a0217b03195676d7468657c46cab44f443d339
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-10-25 15:54:56 +0100

    trippler

--- schema.py
+++ schema.py
@@ -69,7 +69,7 @@ class Query:
                         employee=EmployeeAttendatceType(id=att.employee.id, firstname=att.employee.firstname, lastname=att.employee.lastname),
                         clock_in=att.clock_in_time,
                         clock_out=att.clock_out_time,
-                        time_in_building = calculate_time_in_building(clock_in, clock_out)
+                        time_in_building = calculate_time_in_building(att.clock_in_time.strftime("%H:%M:%S"), att.clock_out_time.strftime("%H:%M:%S"))
                     ) for att in attendances
                 ]
 
