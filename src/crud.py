ca21595ddaf8bdc29bbe6192af4469c728095548
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-08 16:32:07 +0100

    count employees

--- src/crud.py
+++ src/crud.py
@@ -99,7 +99,7 @@ def on_leave_number(db: Session):
     :return: A list of dictionaries, each containing the month, day, week of the year, number of on-time, late, and abscent employees.
     """
     
-    leave_count = db.query(Employee).join(Leave, Employee.id == Leave.employee_id).all()
+    leave_count = db.query(Employee).join(Leave, Employee.id == Leave.employee_id).count()
     print("LEave count", leave_count)
 
 
