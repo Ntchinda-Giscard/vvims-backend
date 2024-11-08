224a61a2eec59bb3fbf50bbd15ad71f8f3ed95c6
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-08 11:59:33 +0100

    allow this

--- schema.py
+++ schema.py
@@ -88,7 +88,7 @@ class Query:
         with next(get_db()) as db:
             try:
                 result  = count_attendance_percentage(db)
-
+                return result
             except Exception as e:
                 logger.error(f"Error while calculating attendance percentage: {e}")
                 raise e
@@ -96,7 +96,7 @@ class Query:
                 db.close()
 
 
-           return 
+            
 
 @strawberry.type
 class Subscription:
