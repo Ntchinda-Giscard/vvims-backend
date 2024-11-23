225266f3db72f4d305c6353defb3839f0a565d22
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-11-23 15:36:11 +0100

    messages error

--- src/models.py
+++ src/models.py
@@ -693,7 +693,7 @@ class GroupMembers(Base):
     group = relationship('Group', back_populates='group_members')
 
 class GroupMessages(Base):
-    __tablename__ = 'group_members'
+    __tablename__ = 'group_messages'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
