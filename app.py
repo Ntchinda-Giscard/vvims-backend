e31dabba5f2278330e05bb7870fde9655ba500f5
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-04 13:33:16 +0100

    remove unwanted print

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ s3 = boto3.client(
     's3',
     aws_access_key_id= os.getenv('AWS_ACCESS_KEY'),
     aws_secret_access_key= os.getenv('AWS_SECRET_KEY'),
-    region_name='eu-north-1'  # Optional
+    region_name='eu-north-1'
 )
 origins= ["172.17.15.42"]
 app = FastAPI(
