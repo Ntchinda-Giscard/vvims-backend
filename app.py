b75563639212e564031d7584270d3c320c263248
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-06 09:23:52 +0100

    remove comma error

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from src.database import engine, get_db
 from src.models import Employee, CompanySettings, Attendance, AttendanceState, AppVersions, UploadedFile, \
     EmployeeNotification, Visit, Visitor, EmployeeNotificationType, EventParticipant, ParticipantStatus, Conversation, \
     EmployeeConversation, Attachment, Message, MessageStatus, MessageStatuses
-from src.schema.input_type import LoginInput,
+from src.schema.input_type import LoginInput
 from src.utils import is_employee_late, PineconeSigleton, upload_to_s3, generate_date_range, get_attendance_for_day, calculate_time_in_building
 import boto3
 
