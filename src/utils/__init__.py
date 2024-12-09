3f5529ff9f433931497da7df79cc35377a5aca2b
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-09 14:42:35 +0100

    import in multiple

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -15,7 +15,7 @@ import google.auth
 from google.oauth2 import service_account
 import google.auth.transport.requests
 
-from src.configs import Strategies, UploadStrategies
+from src.configs import UploadStrategies
 from src.models import Attendance, Employee
 
 def auth_firebase_token() -> str:
