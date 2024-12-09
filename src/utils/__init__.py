0192fe6d04fb15b6b3a71b0b648a62d6d0cdcdd6
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-09 14:40:44 +0100

    solve duplicate circularimport

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import Callable
+
 from fastapi import UploadFile
 from abc import abstractmethod, ABC
 from sqlalchemy import and_
@@ -197,6 +200,24 @@ class LocalUploadStrategy(UploadStrategy):
         return f"Uploaded to Local file: {file}"
 
 
+@dataclass
+class UploadStrategies:
+    online: Callable[[], UploadStrategy]
+    local: Callable[[], UploadStrategy]
+
+    def get_strategy(self, upload_type: str) -> UploadStrategy:
+        """
+        Dynamically fetch the correct strategy based on client input
+        """
+
+        strategy_creator = getattr(self, upload_type, None)
+        if not strategy_creator:
+            raise ValueError(f"Unsupported upload type: {upload_type} ")
+        strategy_instance = strategy_creator()
+        if not isinstance(strategy_instance, UploadStrategy):
+            raise TypeError(f"Strategy is not of type UploadStrategy: {strategy_instance}")
+
+        return strategy_instance
 
 class UploadProcessor:
 
