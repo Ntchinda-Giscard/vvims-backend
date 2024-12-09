453a1c290087abf8d2a9eeb2311c05e465ddf69f
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-12-09 18:27:39 +0100

    commit finally worked

--- app.py
+++ app.py
@@ -441,7 +441,7 @@ async def upload_file_strategy(upload_type:str, file: UploadFile=File(...)):
     strategies = UploadStrategies( local=LocalUploadStrategy, online=S3UploadStrategy)
 
     processor = UploadProcessor(strategies)
-    result = processor.process(upload_type, file)
+    result = await processor.process(upload_type, file)
     print(result)
 
     return "result"
