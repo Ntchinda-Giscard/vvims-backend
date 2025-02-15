51435d2dd120034118011539d4a6fef923bb2759
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-02-15 13:58:42 +0100

    srategy putting

--- src/utilities.py
+++ src/utilities.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass
 import base64
 import boto3
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ from io import BytesIO
 from fastapi.responses import JSONResponse
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
+from abc import ABC, abstractmethod
 
 
 # Set up Jinja2 environment (assuming your templates are in the "templates" folder)
@@ -44,13 +46,6 @@ def render_html(report_data, data_dept, summary, company_name):
     """Render the HTML report using Jinja2 with embedded data and chart."""
     template = env.get_template("reports.html")
     
-    # Sample table data for the report
-    # report_data = [
-    #     {"name": "John Doe", "present": 20, "avr_hrs": 98},
-    #     {"name": "Jane Smith", "present": 18, "avr_hrs": 98},
-    #     {"name": "Michael Brown", "present": 22, "avr_hrs": 98},
-    # ]
-    
     rendered_html = template.render(
         date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         data=report_data,
@@ -68,3 +63,67 @@ def generate_pdf(report_data, data_dept, summary, company_name):
     return pdf_bytes
 
 
+class ReportGeneratorStrategy(ABC):
+
+    @abstractmethod
+    def render_html(self):
+        return NotImplementedError
+
+    @abstractmethod
+    def generate_pdf(self):
+        return NotImplementedError
+
+    @abstractmethod
+    def generate_report(self):
+        return NotImplementedError
+
+
+class VisitsReportGenerator(ReportGeneratorStrategy):
+
+    def __init__(self, report_data, data_dept, summary, company_name):
+        self.report_data = report_data
+        self.data_dept = data_dept
+        self.summary = summary
+        self.company_name = company_name
+
+    def render_html(self):
+        return render_html(self.report_data, self.data_dept, self.summary, self.company_name)
+
+    def generate_pdf(self):
+        return generate_pdf(self.report_data, self.data_dept, self.summary, self.company_name)
+
+    def generate_report(self):
+        return self.generate_pdf()
+
+class AttendanceReportGenerator(ReportGeneratorStrategy):
+
+    def __init__(self, report_data, data_dept, summary, company_name):
+        self.report_data = report_data
+        self.data_dept = data_dept
+        self.summary = summary
+        self.company_name = company_name
+
+    def render_html(self):
+        return render_html(self.report_data, self.data_dept, self.summary, self.company_name)
+
+    def generate_pdf(self):
+        return generate_pdf(self.report_data, self.data_dept, self.summary, self.company_name)
+
+    def generate_report(self):
+        return self.generate_pdf()
+
+
+class ReportGenetorContext:
+
+    def __init__(self, strategy: ReportGeneratorStrategy):
+        self.strategy = strategy
+
+    def generate_report(self):
+        return self.strategy.generate_report()
+
+
+
+@dataclass
+class Report:
+    visit_report: ReportGeneratorStrategy = VisitsReportGenerator()
+    attendance_report: ReportGeneratorStrategy = AttendanceReportGenerator()
