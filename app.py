9a7da9d62840d6154d2be6826e2df04c3647550a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-03-03 15:02:42 +0100

    upload vsitors image

--- app.py
+++ app.py
@@ -486,7 +486,7 @@ async def add_visit_with_visitor(
         file_size = 0
         face_file_url = ''
         face_file_name = ''
-        embedding = None
+        embedding = []
         try:
             mime_type, file_size, face_file_url, face_file_name, file_path = await uploads_save(face, upload_type=upload_type)
             print(f"type=={mime_type}, size=={file_size}, url=={face_file_url}, filename=={face_file_name}, path=={file_path}")
